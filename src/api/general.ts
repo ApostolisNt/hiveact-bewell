bfadae56be7580c0e84cdfd559782522be61fe98
ApostolisNt <apo7ntask@gmail.com>
2025-09-24 13:58:30 +0300

    auth

--- src/api/general.ts
+++ src/api/general.ts
@@ -1,7 +1,7 @@
 import type { FormValues } from "../schema/general";
 
-const LOCAL_API_URL = "http://localhost/escape-public-autohelp-api/api";
-// const API_URL = "https://autohelp-v2.escapegameover.com/api";
+// const LOCAL_API_URL = "http://localhost/escape-public-autohelp-api/api";
+const API_URL = "https://autohelp-v2.escapegameover.com/api";
 // const PREVIEW_URL = "https://preview-autohelp.escapegameover.com/api";
 
 export async function mockPostReaction(payload: {
@@ -31,7 +31,7 @@ export async function loginRequest(payload: {
   password: string;
 }): Promise<{ ok: boolean; data: LoginResponse }> {
   try {
-    const res = await fetch(`${LOCAL_API_URL}/login.php`, {
+    const res = await fetch(`${API_URL}/auth/login.php`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(payload),
